e1442f8ba6ae614d148f61f116c57991c346dc64
wgpdb <dariusz.borek@gmail.com>
2022-09-24 20:40:22 +0200

    added cross origin support

--- src/main/java/com/wgpdb/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/wgpdb/ecommerce/repository/ProductRepository.java
@@ -3,8 +3,10 @@ package com.wgpdb.ecommerce.repository;
 import com.wgpdb.ecommerce.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
