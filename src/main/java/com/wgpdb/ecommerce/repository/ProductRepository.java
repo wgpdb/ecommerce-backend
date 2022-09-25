4443a7026a0baf4ceef34dbfa1d1b4342811d89f
wgpdb <dariusz.borek@gmail.com>
2022-09-26 00:21:43 +0200

    added product search

--- src/main/java/com/wgpdb/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/wgpdb/ecommerce/repository/ProductRepository.java
@@ -13,4 +13,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findByProductCategoryId(@Param("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
